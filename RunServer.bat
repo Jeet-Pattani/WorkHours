20263e2705226e9c29c32189cc3ed4f7f897df11
Keval Indrodia <93391700+Jeet-Pattani@users.noreply.github.com>
2024-02-11 18:04:36 +0530

    2.4

--- RunServer.bat
+++ RunServer.bat
@@ -1,5 +1,7 @@
 @echo off
 cd K:\Fine_Tune_LLM\WorkHours
 start /B node server.js
-timeout /t 2 /nobreak > nul
-start chrome http://localhost:3000/
+timeout /t 1 /nobreak > nul
+# Retrieve the local IP address
+local_ip=$(hostname -I | awk '{print $1}')
+start chrome "http://$local_ip:3000/"
